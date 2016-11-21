456b9e84a77c25d8977c98fc7dc5a7a4efd62aa8
이수진 <@LG>
2016-11-21 23:44:47 +0900

    GameRule update

--- IndianPoker_3/src/Model/IndianPoker.java
+++ IndianPoker_3/src/Model/IndianPoker.java
@@ -40,24 +40,28 @@ public class IndianPoker {
 			}
 			turn++;
 			System.out.println("[Turn : "+turn+"]");
-			
 			p1.card.selectCard();
 			p2.card.selectCard();
 			
-			p1.Betting();
-			p2.Betting();
-			
-			currentBP += p1.betPoint + p2.betPoint; //현재 이 게임에 걸린 베팅점수
-			
-			System.out.println("Player1 카드번호 : "+p1.card.cardValue+", Player1 베팅점수 : "+p1.betPoint); //콘솔에서 잘되나 확인차 출력
-			System.out.println("Player2 카드번호 : "+p2.card.cardValue+", Player2 베팅점수 : "+p2.betPoint); //실제로는 보여주면 안되고 베팅점수만
-			
-			if(p1.betPoint == 0 || p2.betPoint == 0){
-				GiveUp();
-			}else{
-				compare();
+			while(true){
+				p1.Betting();
+				p2.Betting();
+				
+				currentBP += p1.betPoint + p2.betPoint; //현재 이 게임에 걸린 베팅점수
+				
+				System.out.println("Player1 카드번호 : "+p1.card.cardValue+", Player1 베팅점수 : "+p1.betPoint); //콘솔에서 잘되나 확인차 출력
+				System.out.println("Player2 카드번호 : "+p2.card.cardValue+", Player2 베팅점수 : "+p2.betPoint); //실제로는 보여주면 안되고 베팅점수만
+				
+				if(p1.betPoint == 0 || p2.betPoint == 0){
+					GiveUp();
+					break;
+				}
+				
+				if(p1.betPoint == p2.betPoint){
+					compare();
+					break;
+				}
 			}
-			
 			System.out.println(p1.name + "님의 남은 포인트는 " + p1.point + "이고, " + p1.win + "번 이겼습니다.");
 			System.out.println(p2.name + "님의 남은 포인트는 " + p2.point + "이고, " + p2.win + "번 이겼습니다.");
 			System.out.println("------------------------------------------------------");
@@ -65,16 +69,34 @@ public class IndianPoker {
 	}
 	
 	public void GiveUp(){
-		if(p1.betPoint == 0){
+		if(p1.betPoint == 0 && p2.betPoint == 0){ //둘다 포기한 경우
 			if(p1.card.cardValue == 10){
 				p1.point -= 100; //Player1이 포기했는데 카드번호가 10인 경우 100점 차감
+				currentBP += 100;
 			}
-		}
-		if(p2.betPoint == 0){
 			if(p2.card.cardValue == 10){
 				p2.point -= 100; //Player2가 포기했는데 카드번호가 10인 경우 100점 차감
+				currentBP += 100;
+			}
+		}else{
+			if(p1.betPoint == 0){
+				if(p1.card.cardValue == 10){
+					p1.point -= 100; //Player1이 포기했는데 카드번호가 10인 경우 100점 차감
+					currentBP += 100;
+					p2.point += currentBP;
+					currentBP = 0;
+				}
+			}
+			if(p2.betPoint == 0){
+				if(p2.card.cardValue == 10){
+					p2.point -= 100; //Player2가 포기했는데 카드번호가 10인 경우 100점 차감
+					currentBP += 100;
+					p1.point += currentBP;
+					currentBP = 0;
+				}
 			}
 		}
+		
 	}
 	public void compare(){
 		if(p1.card.cardValue > p2.card.cardValue){ //Player1 승리
